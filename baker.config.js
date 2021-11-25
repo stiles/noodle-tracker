3c576be66420d9ce01615859d54299fad0360b96
Stiles <matt.stiles@latimes.com>
2021-11-25 10:16:08 -0800

    bones day!

--- baker.config.js
+++ baker.config.js
@@ -9,14 +9,14 @@ export default {
     entrypoints.length > 1 ? `{${entrypoints.join(',')}}` : entrypoints[0]
   }.js`,
   pathPrefix:
-    process.env.BAKER_PATH_PREFIX || process.env.DELIVERY_BASE_PATH || '/',
-  createPages(createPage, data) {
-    const pageList = data.example;
-    for (const d of pageList) {
-      const template = 'year-detail.html';
-      const url = `${d.year}`;
-      const context = { obj: d };
-      createPage(template, url, context);
-    }
-  },
+    process.env.BAKER_PATH_PREFIX || process.env.DELIVERY_BASE_PATH || '/'
+  // createPages(createPage, data) {
+  //   const pageList = data.example;
+  //   for (const d of pageList) {
+  //     const template = 'year-detail.html';
+  //     const url = `${d.year}`;
+  //     const context = { obj: d };
+  //     createPage(template, url, context);
+  //   }
+  // },
 };
