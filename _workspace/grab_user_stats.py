25d7a9b162c7996f919618d10ead9155277448f2
Matt Stiles <mattstiles@gmail.com>
2021-12-18 20:38:15 -0800

    latest

---
+++ _workspace/grab_user_stats.py
@@ -0,0 +1,34 @@
+from TikTokApi import TikTokApi
+import json
+import pandas as pd
+
+verifyFp = "verify_kvpkl15s_dxOlRDtv_BOpy_4A2Z_9eQb_uGluOSC3ohe3"
+
+api = TikTokApi.get_instance(custom_verifyFp = verifyFp)
+
+count = 200
+
+username = "mattystiles"
+
+user_videos = api.by_username(username, count=count)
+
+def simple_dict(tiktok_dict):
+  to_return = {}
+  to_return['user_name'] = tiktok_dict['author']['uniqueId']
+  to_return['user_id'] = tiktok_dict['author']['id']
+  to_return['video_id'] = tiktok_dict['id']
+  to_return['video_desc'] = tiktok_dict['desc']
+  to_return['video_time'] = tiktok_dict['createTime']
+  to_return['video_length'] = tiktok_dict['video']['duration']
+  to_return['video_link'] = 'https://www.tiktok.com/@{}/video/{}?lang=en'.format(to_return['user_name'], to_return['video_id'])
+  to_return['n_likes'] = tiktok_dict['stats']['diggCount']
+  to_return['n_shares'] = tiktok_dict['stats']['shareCount']
+  to_return['n_comments'] = tiktok_dict['stats']['commentCount']
+  to_return['n_plays'] = tiktok_dict['stats']['playCount']
+  return to_return
+
+user_videos = [simple_dict(v) for v in user_videos]
+user_videos_df = pd.DataFrame(user_videos)
+user_videos_df.to_csv('{}_videos.csv'.format(username),index=False)
+
+print(user_videos_df['n_plays'].sum())
